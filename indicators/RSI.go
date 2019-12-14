576fb5048363d18248cb68954c2ba225fc944ced
NERON <neronium1913@gmail.com>
2019-12-14 13:26:14 +0300

    initial commit

---
+++ indicators/RSI.go
@@ -0,0 +1,66 @@
+package indicators
+
+import "math"
+
+type RSI struct {
+	Period uint
+
+	avgGain float64
+	avgLoss float64
+
+	pointsCount uint
+	lastValue   float64
+}
+
+func (rsi *RSI) AddPoint(value float64) {
+
+	rsi.pointsCount++
+
+	if rsi.pointsCount > 1 && rsi.pointsCount <= rsi.Period+1 {
+
+		rsi.avgGain += math.Max(value - rsi.lastValue,0)
+		rsi.avgLoss += math.Max(rsi.lastValue - value,0)
+
+		if rsi.pointsCount == rsi.Period + 1 {
+
+			rsi.avgGain /= float64(rsi.Period)
+			rsi.avgLoss /= float64(rsi.Period)
+		}
+
+	} else if rsi.pointsCount > rsi.Period+1 {
+
+		rsi.avgGain = (float64(rsi.Period-1) * rsi.avgGain + math.Max(value - rsi.lastValue,0)) / float64(rsi.Period)
+		rsi.avgLoss = (float64(rsi.Period-1) * rsi.avgLoss + math.Max(rsi.lastValue - value,0)) / float64(rsi.Period)
+	}
+
+	rsi.lastValue = value
+
+}
+
+func (rsi *RSI) Calculate() (float64,bool){
+
+	if rsi.pointsCount < rsi.Period + 1 {
+		return 0,false
+	}
+
+	return 100 - 100 / ( 1  + rsi.avgGain / rsi.avgLoss), true
+}
+func (rsi *RSI) PredictForNextPoint(value float64) (float64,bool) {
+
+	avgG := rsi.avgGain
+	avgL := rsi.avgLoss
+	pC := rsi.pointsCount
+	lV := rsi.lastValue
+
+	rsi.AddPoint(value)
+
+	result, notNaN := rsi.Calculate()
+
+	rsi.avgGain = avgG
+	rsi.avgLoss = avgL
+	rsi.pointsCount = pC
+	rsi.lastValue = lV
+
+	return result,notNaN
+
+}
---
+++ main.go
@@ -0,0 +1,250 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/lib/pq"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"./indicators"
+)
+
+var DatabaseManager *sql.DB
+
+type KLine struct {
+	Symbol                   string
+	OpenTime                 uint64
+	CloseTime                uint64
+	OpenPrice                float64
+	ClosePrice               float64
+	HighPrice                float64
+	LowPrice                 float64
+	BaseVolume               float64
+	QuoteVolume              float64
+	TakerBuyBaseVolume       float64
+	TakerBuyQuoteVolume      float64
+	PrevCloseCandleTimestamp uint64
+}
+
+func toFloat(value string) float64 {
+
+	val, err := strconv.ParseFloat(value, 64)
+
+	if err != nil {
+
+		log.Fatal(err.Error())
+	}
+
+	return val
+}
+
+func GetKlines(symbol string, interval string, startTimestamp uint64, endTimestamp uint64) []KLine {
+
+	result := make([]KLine, 0)
+
+	for i := 0; i < 2; i++ {
+
+		urlS := fmt.Sprintf("https://api.binance.com/api/v1/klines?symbol=%s&interval=%s&limit=1000", symbol, interval)
+
+		if endTimestamp > 0 {
+
+			urlS = fmt.Sprintf(urlS+"&endTime=%d", endTimestamp)
+		}
+
+		if startTimestamp > 0 {
+
+			urlS = fmt.Sprintf(urlS+"&startTime=%d", startTimestamp)
+		}
+
+		resp, err := http.Get(urlS)
+
+		if err != nil {
+
+			log.Fatal("Get error: ", err.Error())
+		}
+
+		log.Println("CALL API")
+
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+
+		klines := make([]interface{}, 0)
+
+		err = json.Unmarshal(body, &klines)
+
+		if err != nil {
+
+			log.Fatal("Get error: ", err.Error())
+		}
+
+		var prevKline *KLine = nil
+
+		for j := len(klines) - 1; j > 0; j-- {
+
+			data := klines[j]
+
+			k := data.([]interface{})
+
+			kline := KLine{}
+			kline.Symbol = symbol
+			kline.OpenTime = uint64(k[0].(float64))
+			kline.OpenPrice = toFloat(k[1].(string))
+			kline.HighPrice = toFloat(k[2].(string))
+			kline.LowPrice = toFloat(k[3].(string))
+			kline.ClosePrice = toFloat(k[4].(string))
+			kline.BaseVolume = toFloat(k[5].(string))
+			kline.CloseTime = uint64(k[6].(float64))
+			kline.QuoteVolume = toFloat(k[7].(string))
+			kline.TakerBuyBaseVolume = toFloat(k[9].(string))
+			kline.TakerBuyQuoteVolume = toFloat(k[10].(string))
+
+			if prevKline != nil {
+
+				result[len(result)-1].PrevCloseCandleTimestamp = kline.CloseTime
+			}
+
+			prevKline = &kline
+			result = append(result, kline)
+
+		}
+
+		endTimestamp = result[len(result)-1].OpenTime - 1
+		log.Println("SUCCESS API", endTimestamp)
+
+	}
+
+	itemCount := len(result)
+
+	for i := 0; i < itemCount/2; i++ {
+
+		mirrorIdx := itemCount - i - 1
+		result[i], result[mirrorIdx] = result[mirrorIdx], result[i]
+
+	}
+
+	return result
+
+}
+func SaveCandles() {
+	klines := GetKlines("BTCUSDT", "1h", 0, 0)
+
+	stmt, err := DatabaseManager.Prepare(`INSERT INTO public.candles_data(
+	"Symbol", "Interval", "OpenTime", "CloseTime", "OpenPrice", "ClosePrice", "LowPrice", "HighPrice", "Volume", "QuoteVolume", "TakerVolume", "TakerQuoteVolume", "PrevCandleCloseTime", "UniqueID")
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13,DEFAULT) ON CONFLICT DO NOTHING;`)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	for _, kline := range klines {
+		t := time.Now()
+		_, err = stmt.Exec(kline.Symbol, 60, time.Unix(0, int64(kline.OpenTime)*int64(1000000)).UTC(), time.Unix(0, int64(kline.CloseTime)*int64(1000000)).UTC(), kline.OpenPrice, kline.ClosePrice, kline.LowPrice, kline.HighPrice, kline.BaseVolume, kline.QuoteVolume, kline.TakerBuyBaseVolume, kline.TakerBuyQuoteVolume, time.Unix(0, int64(kline.PrevCloseCandleTimestamp)*int64(1000000)).UTC())
+		log.Println(time.Since(t))
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+	}
+	stmt.Close()
+}
+func LoadCandles(symbol string, interval uint) ([]KLine, error) {
+
+	var rows, err = DatabaseManager.Query(`SELECT  
+										 "Symbol", 
+										 extract(epoch from "OpenTime")::bigint * 1000, 
+										 extract(epoch from "CloseTime")::bigint * 1000, 
+										 "OpenPrice", 
+										 "ClosePrice", 
+										 "LowPrice", 
+										 "HighPrice", 
+										 "Volume", 
+										 "QuoteVolume", 
+										 "TakerVolume", 
+										 "TakerQuoteVolume", 
+										 extract(epoch from "PrevCandleCloseTime")::bigint * 1000
+										 FROM public.candles_data 
+										 WHERE "Symbol" = $1 AND "Interval" = $2
+										 ORDER BY "OpenTime" ASC`,symbol,interval)
+
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]KLine,0)
+
+	for rows.Next() {
+
+		kline := KLine{}
+
+		err = rows.Scan(&kline.Symbol,
+			      &kline.OpenTime,
+			      &kline.CloseTime,
+			      &kline.OpenPrice,
+			      &kline.ClosePrice,
+			      &kline.LowPrice,
+			      &kline.HighPrice,
+			      &kline.BaseVolume,
+			      &kline.QuoteVolume,
+			      &kline.TakerBuyBaseVolume,
+			      &kline.TakerBuyQuoteVolume,
+			      &kline.PrevCloseCandleTimestamp)
+
+		if err != nil {
+
+			rows.Close()
+			return nil,err
+		}
+		log.Println(kline)
+
+		result  = append(result, kline)
+	}
+
+	rows.Close()
+
+	return result,nil
+
+
+
+}
+
+func OpenDatabaseConnection() error {
+
+	var err error
+	DatabaseManager, err = sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@157.230.174.164/%s", "neronru", "TESTSHIT", "trades"))
+
+	return err
+}
+
+func main() {
+
+	err := OpenDatabaseConnection()
+
+	if err != nil {
+
+		log.Fatal("Database connection error: ", err.Error())
+	}
+
+	SaveCandles()
+
+	rsi:= indicators.RSI{Period:14}
+
+	klines,err := LoadCandles("BTCUSDT",60)
+
+	for _,kline :=range klines {
+
+		rsi.AddPoint(kline.ClosePrice)
+		calcRSI,isNotNaN := rsi.Calculate()//rsi.PredictForNextPoint(kline.ClosePrice)
+
+		log.Println(kline.OpenTime,kline.ClosePrice,calcRSI,isNotNaN)
+
+	}
+
+
+
+
+}
