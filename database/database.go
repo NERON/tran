ee09eb865c7926dd33d662cd86ead9929bc735b3
NERON <neronium1913@gmail.com>
2020-10-17 06:28:14 +0300

    change port database

--- database/database.go
+++ database/database.go
@@ -10,7 +10,7 @@ var DatabaseManager *sql.DB
 func OpenDatabaseConnection() error {
 
 	var err error
-	DatabaseManager, err = sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@127.0.0.1:1235/%s", "neron", "neronium196531abc", "tran"))
+	DatabaseManager, err = sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@127.0.0.1:5432/%s", "neron", "neronium196531abc", "tran"))
 
 	return err
 }
