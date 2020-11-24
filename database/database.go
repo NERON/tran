e44d2a8ce541cb449e7632038078f18065bd92d1
NERON <neronium1913@gmail.com>
2020-11-24 22:16:59 +0300

    fix some shit

--- database/database.go
+++ database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log"
 )
 
 var DatabaseManager *sql.DB
@@ -10,6 +11,7 @@ var DatabaseManager *sql.DB
 func OpenDatabaseConnection() error {
 
 	var err error
+	log.Println("establish db")
 	DatabaseManager, err = sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@127.0.0.1:5432/%s", "neron", "neronium196531abc", "tran"))
 
 	return err
