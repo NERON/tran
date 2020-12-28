a6c11129b0b7228375738ae74e0d8af21b6d2ab4
NERON <neronium1913@gmail.com>
2020-12-28 08:54:35 +0300

    fix

--- indicators/RSILowReverse.go
+++ indicators/RSILowReverse.go
@@ -1,16 +1,14 @@
 package indicators
 
 type rsiLowReverse struct {
-
 	lastRSIValues []float64
 }
 
-func (r *rsiLowReverse) AddPoint(calcValue float64,addValue float64) {
+func (r *rsiLowReverse) AddPoint(calcValue float64, addValue float64) {
 
 	r.lastRSIValues[0] = r.lastRSIValues[1]
 	r.lastRSIValues[1] = r.lastRSIValues[2]
 	r.lastRSIValues[2] = calcValue
-
 
 }
 
@@ -21,13 +19,13 @@ func (r *rsiLowReverse) IsPreviousLow() bool {
 		return false
 	}
 
-	return r.lastRSIValues[1]  <= r.lastRSIValues[0]  && r.lastRSIValues[1] <= r.lastRSIValues[2]
+	return r.lastRSIValues[1] <= r.lastRSIValues[0] && r.lastRSIValues[1] < r.lastRSIValues[2]
 }
 
 func NewRSILowReverseIndicator() *rsiLowReverse {
 
-	lastValues := []float64{-1,-1,-1}
+	lastValues := []float64{-1, -1, -1}
 
-	return &rsiLowReverse{ lastRSIValues: lastValues}
+	return &rsiLowReverse{lastRSIValues: lastValues}
 
 }
