1bcd813ec8fa5fd0341e42d4f8a35c26d3a85593
NERON <neronium1913@gmail.com>
2021-02-04 17:19:56 +0300

    fix when api result close time < open time

--- providers/binance.go
+++ providers/binance.go
@@ -36,13 +36,6 @@ func GetSupportedTimeframes() map[string][]uint {
 	}
 }
 
-func NewStandardBinanceProvider() BinanceProvider {
-
-	return BinanceProvider{
-		baseUrl: "https://api.binance.com/",
-	}
-}
-
 func (provider *BinanceProvider) GetServerTime() time.Time {
 
 	return time.Now()
@@ -59,109 +52,6 @@ func toFloat(value string) float64 {
 	}
 
 	return val
-}
-
-func GetKlines(symbol string, interval string, startTimestamp uint64, endTimestamp uint64, reverseOrder bool) []candlescommon.KLine {
-
-	result := make([]candlescommon.KLine, 0)
-
-	for i := 0; i < 3; i++ {
-
-		urlS := fmt.Sprintf("https://api.binance.com/api/v1/klines?symbol=%s&interval=%s&limit=1000", symbol, interval)
-
-		if endTimestamp > 0 {
-
-			urlS = fmt.Sprintf(urlS+"&endTime=%d", endTimestamp)
-		}
-
-		if startTimestamp > 0 {
-
-			urlS = fmt.Sprintf(urlS+"&startTime=%d", startTimestamp)
-		}
-
-		resp, err := http.Get(urlS)
-
-		if err != nil {
-
-			log.Fatal("Get error: ", err.Error())
-		}
-
-		body, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-
-		klines := make([]interface{}, 0)
-
-		err = json.Unmarshal(body, &klines)
-
-		log.Println("klines length", len(klines))
-
-		if err != nil {
-
-			log.Fatal("Get error: ", err.Error())
-		}
-
-		for j := len(klines) - 1; j > 0; j-- {
-
-			data := klines[j]
-
-			k := data.([]interface{})
-
-			kline := candlescommon.KLine{}
-			kline.Symbol = symbol
-			kline.OpenTime = uint64(k[0].(float64))
-			kline.OpenPrice = toFloat(k[1].(string))
-			kline.HighPrice = toFloat(k[2].(string))
-			kline.LowPrice = toFloat(k[3].(string))
-			kline.ClosePrice = toFloat(k[4].(string))
-			kline.BaseVolume = toFloat(k[5].(string))
-			kline.CloseTime = uint64(k[6].(float64))
-			kline.QuoteVolume = toFloat(k[7].(string))
-			kline.TakerBuyBaseVolume = toFloat(k[9].(string))
-			kline.TakerBuyQuoteVolume = toFloat(k[10].(string))
-			kline.Closed = true
-
-			if len(result) > 0 {
-
-				result[len(result)-1].PrevCloseCandleTimestamp = kline.CloseTime
-			}
-
-			result = append(result, kline)
-
-		}
-
-		if len(result) == 0 {
-			break
-		}
-
-		endTimestamp = result[len(result)-1].OpenTime - 1
-
-	}
-
-	if !reverseOrder {
-
-		itemCount := len(result)
-
-		for i := 0; i < itemCount/2; i++ {
-
-			mirrorIdx := itemCount - i - 1
-			result[i], result[mirrorIdx] = result[mirrorIdx], result[i]
-
-		}
-
-		if len(result) > 0 {
-			result[len(result)-1].Closed = false
-		}
-
-	} else {
-
-		if len(result) > 0 {
-			result[0].Closed = false
-		}
-
-	}
-
-	return result
-
 }
 
 func GetLastKlines(symbol string, interval string) ([]candlescommon.KLine, error) {
@@ -191,17 +81,15 @@ func GetLastKlines(symbol string, interval string) ([]candlescommon.KLine, error
 
 func GetKlinesNew(symbol string, interval string, ranges GetKlineRange) ([]candlescommon.KLine, error) {
 
-	log.Println("fetching data...", interval, ranges)
-
 	t := time.Now()
 
 	klines, err := getKline(symbol, interval, ranges)
 
-	log.Println("API GetKLinesNew time ", time.Since(t))
-
 	if err != nil {
 		return nil, err
 	}
+
+	log.Println("API GetKLinesNew time ", interval, ranges, time.Since(t))
 
 	if len(klines) == 0 {
 		return klines, nil
@@ -215,9 +103,7 @@ func GetKlinesNew(symbol string, interval string, ranges GetKlineRange) ([]candl
 	//if kline with smallest open time has prevCloseCandle equals 0, we should remove them
 	if len(klines) > 0 && klines[len(klines)-1].PrevCloseCandleTimestamp == 0 {
 		klines = klines[:len(klines)-1]
-	}
-
-	if len(klines) > 0 && klines[len(klines)-1].PrevCloseCandleTimestamp == math.MaxUint64 {
+	} else if len(klines) > 0 && klines[len(klines)-1].PrevCloseCandleTimestamp == math.MaxUint64 {
 		klines[len(klines)-1].PrevCloseCandleTimestamp = 0
 	}
 
@@ -288,6 +174,10 @@ func getKline(symbol string, interval string, ranges GetKlineRange) ([]candlesco
 			result[len(result)-1].PrevCloseCandleTimestamp = kline.CloseTime
 		}
 
+		if kline.OpenTime > kline.CloseTime {
+			kline.CloseTime = kline.OpenTime
+		}
+
 		result = append(result, kline)
 
 	}
@@ -303,51 +193,5 @@ func getKline(symbol string, interval string, ranges GetKlineRange) ([]candlesco
 	}
 
 	return result, nil
-
-}
-
-func GetAvailableSymbols() ([]string, error) {
-
-	resp, err := http.Get("https://api.binance.com/api/v3/exchangeInfo")
-
-	if err != nil {
-		return nil, err
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return nil, err
-	}
-
-	err = resp.Body.Close()
-
-	if err != nil {
-		return nil, err
-	}
-
-	type SymbolInfoJSON struct {
-		Symbol string `json:"symbol"`
-	}
-
-	type ExchangeInfoJSON struct {
-		Symbols []SymbolInfoJSON `json:"symbols"`
-	}
-
-	exchangeInfo := ExchangeInfoJSON{}
-
-	err = json.Unmarshal(body, &exchangeInfo)
-
-	if err != nil {
-		return nil, err
-	}
-
-	symbolsNames := make([]string, 0)
-
-	for _, symbol := range exchangeInfo.Symbols {
-		symbolsNames = append(symbolsNames, symbol.Symbol)
-	}
-
-	return symbolsNames, nil
 
 }
