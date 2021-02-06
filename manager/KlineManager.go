e0d8191c61d41d0028a16b1188f6668e427dd560
NERON <neronium1913@gmail.com>
2021-02-06 10:12:56 +0300

    new kline grouping

--- manager/KlineManager.go
+++ manager/KlineManager.go
@@ -249,13 +249,13 @@ func GetLastKLinesFromTimestamp(symbol string, interval candlescommon.Interval, 
 				return nil, err
 			}
 
-			if len(fetchedKlines) == 0 && min == 0 {
+			if len(fetchedKlines) == 0 {
 				break
 			}
 
 			fetchedKlines = convertKlinesToNewTimestamp(fetchedKlines, interval)
 
-			if len(fetchedKlines) == 0 {
+			if len(fetchedKlines) == 0 && min != 0 {
 				FillDatabaseWithPrevValues(symbol, databaseIn, 900)
 				continue
 			}
