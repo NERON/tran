aa28bbc4ec71118ed05d28c8a8de14d1106d0162
NERON <neronium1913@gmail.com>
2021-02-21 03:42:26 +0300

    test

--- http_handlers.go
+++ http_handlers.go
@@ -265,7 +265,10 @@ func ChartUpdateHandler(w http.ResponseWriter, r *http.Request) {
 
 		if lowReverse.IsPreviousLow() {
 			lowsMap[idx-1] = struct{}{}
-		} else if candle.OpenPrice < candle.ClosePrice && candle.LowPrice < candle.OpenPrice {
+		}
+
+		if candle.OpenPrice < candle.ClosePrice && candle.LowPrice < candle.OpenPrice {
+
 			lowsMap[idx] = struct{}{}
 		}
 
