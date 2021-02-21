57594fac49b335dcf60ac42a8bdcb9ef3c69a2ec
NERON <neronium1913@gmail.com>
2021-02-21 03:49:45 +0300

    test

--- http_handlers.go
+++ http_handlers.go
@@ -604,7 +604,10 @@ func SaveCandlesHandler(w http.ResponseWriter, r *http.Request) {
 
 			if lowReverse.IsPreviousLow() {
 				lowsMap[idx-1] = struct{}{}
-			} else if idx > 0 && candle.OpenPrice < candle.ClosePrice && candles[idx-1].LowPrice >= candle.LowPrice {
+			}
+
+			if candle.OpenPrice < candle.ClosePrice && candle.LowPrice < candle.OpenPrice {
+
 				lowsMap[idx] = struct{}{}
 			}
 
