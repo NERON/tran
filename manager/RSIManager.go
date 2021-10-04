74bf1496bce55748b68645e8f507cc46e8e65133
NERON <neronium1913@gmail.com>
2021-10-04 08:09:37 +0300

    test

--- manager/RSIManager.go
+++ manager/RSIManager.go
@@ -76,7 +76,7 @@ func GenerateMapOfPeriods(symbol string, interval candlescommon.Interval, endTim
 
 					percentage := (down/up - 1) * 100
 
-					if candle.LowPrice <= up && candle.LowPrice >= down {
+					if candle.LowPrice <= up {
 
 						_, ok1 := currentPeriods[cR][bestPeriod]
 						_, ok2 := currentPeriods[cR][bestPeriod-1]
