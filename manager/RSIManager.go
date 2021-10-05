cb7bca644622ddfda3c49eecd208751418cc24d1
NERON <neronium1913@gmail.com>
2021-10-05 08:30:59 +0300

    test

--- manager/RSIManager.go
+++ manager/RSIManager.go
@@ -78,10 +78,10 @@ func GenerateMapOfPeriods(symbol string, interval candlescommon.Interval, endTim
 
 					if bestPeriod > 2 || (bestPeriod == 2 && candle.LowPrice <= 2) {
 
-						filledPercentage := (up - candle.LowPrice) / (up - down) * 100
+						//filledPercentage := (up - candle.LowPrice) / (up - down) * 100
 
-						if filledPercentage > 90 {
-							//bestPeriod = bestPeriod + 1
+						if candle.OpenTime == 1602849600000 {
+							bestPeriod = bestPeriod + 1
 							//log.Println(time.Unix(int64(candle.OpenTime/1000), 0), bestPeriod, filledPercentage)
 						}
 
