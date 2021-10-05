6db6c43679ca276bad4f6d53c2a2502c4df89022
NERON <neronium1913@gmail.com>
2021-10-05 08:24:44 +0300

    test

--- manager/RSIManager.go
+++ manager/RSIManager.go
@@ -5,7 +5,6 @@ import (
 	"github.com/NERON/tran/indicators"
 	"log"
 	"math"
-	"time"
 )
 
 type SequenceItemData struct {
@@ -82,7 +81,8 @@ func GenerateMapOfPeriods(symbol string, interval candlescommon.Interval, endTim
 						filledPercentage := (up - candle.LowPrice) / (up - down) * 100
 
 						if filledPercentage > 90 {
-							log.Println(time.Unix(int64(candle.OpenTime/1000), 0), bestPeriod, filledPercentage)
+							bestPeriod = bestPeriod + 1
+							//og.Println(time.Unix(int64(candle.OpenTime/1000), 0), bestPeriod, filledPercentage)
 						}
 
 						_, ok1 := currentPeriods[cR][bestPeriod]
